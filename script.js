464052587c996d3481af9d949726abfc3c60a2a1
Sean <seanacousins@gmail.com>
2023-04-11 22:16:02 -0500

    add click count functionality

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let button = document.getElementById('button');
-let count = document.getElementsByClassName('click-count');
+var button = document.getElementById('button');
+
+var clicks = 0
+function countUp() {
+    clicks+=1;
+    document.getElementById("click-count").innerHTML = 'I\'ve been pressed '+clicks+ ' times!!';
+};
+
+button.addEventListener('click', countUp);
